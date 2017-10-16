ae8a08075fabcde52eb6b2744a7ebb9934ebd8aa
Andrew <andrew.ventura91@gmail.com>
2017-10-15 22:24:05 -0700

    probably broke it

--- Dealer/files/google_script_form_ajax.js
+++ Dealer/files/google_script_form_ajax.js
@@ -6,8 +6,7 @@ $('#u6574-4').on('click', function(e) {
   var jqxhr = $.ajax({
     url: 'https://script.google.com/macros/s/AKfycbyoxd0GPJBYjDYC88JWTx-JyZMfday6mvMF9YLfhTMJGSAaV7U/exec',
     method: "GET",
-    dataType: "json",
+    dataType: "jsonp",
     data: $form.serializeObject(),
-    success:
   })
 })
